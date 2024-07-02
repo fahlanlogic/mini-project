376f23853d1b9424faf03514463fe996bcfe626f
fahlan logic <alanfahdi@gmail.com>
2024-07-02 12:00:13 +0700

    test response api

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 require("dotenv").config();
-
+const express = require("express");
 const { Sequelize } = require("sequelize");
+const router = require("./src/routes");
+
+const app = express();
+const port = 5000;
+const cors = require("cors");
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
 
 // Konfigurasi Sequelize
 const sequelize = new Sequelize(
