c889c628d6ed7dbfa1f8d92f1abce9c4b9ef9526
fahlan logic <alanfahdi@gmail.com>
2024-07-02 16:59:57 +0700

    error handling dueDate

--- src/controllers/tasks/index.js
+++ src/controllers/tasks/index.js
@@ -12,10 +12,17 @@ class TodoControllers {
 
   static create = async (req, res) => {
     try {
-      const { title, description } = req.body;
+      const { title, description, dueDate } = req.body;
+      const now = new Date();
+      const dueDateObj = new Date(dueDate);
 
       if (title.trim() === "" || description.trim() === "") {
         return res.status(400).json({ message: "Please check your input" });
+      }
+      if (dueDateObj < now) {
+        return res
+          .status(400)
+          .json({ message: "Due date cannot be in the past" });
       }
 
       const newtask = await Tasks.create(req.body);
